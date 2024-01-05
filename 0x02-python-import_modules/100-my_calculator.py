93bd00063ccb103009e4e7a8add8a1a0b6f8da40
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-01-05 05:49:12 +0100

    Added files for Task 100, 101, 102 and 103

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, div, mul
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    if argc != 4:
+        print("usage: {} <a> <operator> <b>".format(sys.argv[0]), end="\n")
+        sys.exit(1)
+
+    operator = sys.argv[2]
+    if operator == '+':
+        result = add(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], result), end="\n")
+    elif operator == '-':
+        result = sub(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], result), end="\n")
+    elif operator == '*':
+        result = mul(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], result), end="\n")
+    elif operator == '/':
+        result = div(int(sys.argv[1]), int(sys.argv[3]))
+        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], result), end="\n")
+    else:
+        print("unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
