4c8ed616d900d504a9383176d1a9e75e17d58ce4
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-01-05 05:55:07 +0100

    Fixed bug in 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
         print("{} - {} = {}".format(sys.argv[1], sys.argv[3], result), end="\n")
     elif operator == '*':
         result = mul(int(sys.argv[1]), int(sys.argv[3]))
-        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], result), end="\n")
+        print("{} * {} = {}".format(sys.argv[1], sys.argv[3], result), end="\n")
     elif operator == '/':
         result = div(int(sys.argv[1]), int(sys.argv[3]))
-        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], result), end="\n")
+        print("{} / {} = {}".format(sys.argv[1], sys.argv[3], result), end="\n")
     else:
         print("unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
