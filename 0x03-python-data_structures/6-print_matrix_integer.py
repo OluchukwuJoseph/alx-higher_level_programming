059f3cffbc4f81cf43e6ee9aebc513850af10a04
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-01-08 16:23:53 +0100

    Fixed bug in Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(x), end="")
             else:
                 print("{:d} ".format(x), end="")
-        print("$")
+        print()
