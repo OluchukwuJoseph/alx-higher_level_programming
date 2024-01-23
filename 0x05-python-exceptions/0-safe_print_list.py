e3b7c247592d324846787042cc4ace3c4b1e7db8
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-01-23 05:52:55 +0100

    Fixed bug in Task 4

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for index in range(x):
+    for index in range(x):
+        try:
             print("{}".format(my_list[index]), end="")
-        print()
-        return index + 1
-    except (IndexError, TypeError):
-        print()
-        return index
+        except IndexError:
+            break
+    print()
+    return index
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,18 +4,18 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(list_length):
         try:
-            new_list += [my_list_1[i] / my_list_2[i], ]
+            div = my_list_1[i] / my_list_2[i]
         except Exception as error:
             if isinstance(error, ZeroDivisionError):
-                new_list += [0, ]
+                div = 0
                 print("division by 0")
-                continue
             elif isinstance(error, TypeError):
-                new_list += [0, ]
+                div = 0
                 print("wrong type")
             elif isinstance(error, IndexError):
                 print("out of range")
-                for i in range(list_length - len(new_list)):
-                    new_list += [0, ]
+                div = 0
                 break
+        finally:
+            new_list += [div, ]
     return new_list
