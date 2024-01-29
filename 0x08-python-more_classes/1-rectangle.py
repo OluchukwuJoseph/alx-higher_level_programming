f0a532a34dc0033b3d0829246e3e75260ceff8cf
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-01-29 22:18:03 +0100

    Fixed bug in Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,17 +5,20 @@
 class Rectangle:
     """A class that has instance attributes and methods"""
     def __init__(self, width=0, height=0):
-        self.width(width)
-        self.height(height)
+        self.width = width
+        self.height = height
 
+    @property
     def width(self):
         """Retrieves the private instance attribute width"""
-        return self.width
+        return self.__width
 
+    @property
     def height(self):
         """Retrieves the private instance attribute height"""
-        return self.height
+        return self.__height
 
+    @width.setter
     def width(self, value):
         """sets the private instance attribute width
 
@@ -30,13 +33,14 @@ class Rectangle:
             if value < 0:
                 raise ValueError("width must be >= 0")
             else:
-                self.width = value
+                self.__width = value
         except Exception as error:
             if isinstance(error, TypeError):
                 raise TypeError("width must be an integer")
             else:
                 print(f"An unknown error {error} occured")
 
+    @height.setter
     def height(self, value):
         """sets the private instance attribute height
 
@@ -51,7 +55,7 @@ class Rectangle:
             if value < 0:
                 raise ValueError("height must be >= 0")
             else:
-                self.height = value
+                self.__height = value
         except Exception as error:
             if isinstance(error, TypeError):
                 raise TypeError("height must be an integer")
