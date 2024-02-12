18633f0edc6cb28a1631dfe8e37f66673f4ddeb4
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-02-12 11:46:30 +0100

    Added files for Task 0, 1, 2 and 3

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module contains the Base class
+   models/base.py
+"""
+
+
+class Base:
+    """
+        This class has one method, one class private variable and
+        one instance public variable.
+        They names and types of these variables are:
+            - __nb_objects (int) -> class private variable
+            - id (int) -> instance public variable
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+            initialization method
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+"""This model contains the Rectangle class
+   models/rectangle.py
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+        A class that inherits Base
+        This class has 9 public methods an initialization method, 4 getter
+        methods, and 4 setter method.
+        It also has 5 private instance variables, which are:
+            - width (int)
+            - height (int)
+            - y (int)
+            - x (int)
+            - id (int) -> parent class variable
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+            Intialization method
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+            width getter method
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            width setter method
+        """
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+            height getter method
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            height setter method
+        """
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+            x getter method
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+            x setter method
+        """
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+            y getter method
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+            y setter method
+        """
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
