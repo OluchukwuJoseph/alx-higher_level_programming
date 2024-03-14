7c9fd55864607c59d73f6fb6b862c476f1bf1c3c
Oluchukwu Joseph <devjoseph016@gmail.com>
2024-03-14 20:09:55 +0100

    Added files for Task 6 and 7

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- This script prints all rows of first_table
+-- list row
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- This script inserts a new row in first_table
+-- inserts row
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
