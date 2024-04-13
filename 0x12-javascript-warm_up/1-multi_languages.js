1fe7057b420bac0243e840d0e6057bffde9981aa
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-04-13 17:30:38 +0100

    Added files for Task 5 and 6

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -4,4 +4,6 @@
  * This script prints 3 lines
  */
 
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
