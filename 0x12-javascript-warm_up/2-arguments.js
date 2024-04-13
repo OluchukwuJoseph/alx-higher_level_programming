be56b7d863abbf01d8a88a573770be0e970db414
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-04-13 13:11:14 +0100

    Added files for Task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/*
+ * This script prints a message depending of the number of arguments passed
+ */
+
+const args = process.argv;
+
+if (args.length > 2) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
