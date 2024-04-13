693c4532ac8f5699db62ef2448897dbddc62dc8a
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-04-13 18:14:44 +0100

    Added files for Task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const firstArg = parseInt(process.argv[2]);
+const secondArg = parseInt(process.argv[3]);
+
+function add (a, b) {
+  return a + b;
+}
+
+if (isNaN(firstArg) || isNaN(secondArg)) {
+  console.log('NaN');
+} else {
+  console.log(add(firstArg, secondArg));
+}
