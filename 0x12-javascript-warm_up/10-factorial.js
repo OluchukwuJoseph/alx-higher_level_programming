cce95c356d1fdc24fed4a7a96fd2a27c5f591b8d
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-04-14 08:20:18 +0100

    Added files for Task 11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,10 +4,10 @@ const firstArg = parseInt(process.argv[2]);
 
 function factorial (num) {
   if (num === 0) {
-    return 0;
+    return 1;
   }
 
-  return num + factorial(num - 1);
+  return num * factorial(num - 1);
 }
 
 if (isNaN(firstArg)) {
