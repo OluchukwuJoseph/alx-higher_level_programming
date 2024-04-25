d3aa805918ddf2c9ee48adb989183033e147ae45
Joseph <devjoseph016@gmail.com>
2024-04-25 15:20:51 +0100

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/1-create_user.sql
+++
@@ -1,9 +0,0 @@
--- This script creates the MySQL server user user_0d_1
--- It also grants all privileges to user_0d_1
-
-CREATE USER
-IF NOT EXISTS
-  'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++
@@ -1,8 +0,0 @@
--- This script lists all shows from hbtn_0d_tvshows_rate by their rating.
-
-SELECT shows.title, SUM(show_ratings.rate) AS 'rating'
-FROM tv_shows AS shows
-INNER JOIN tv_show_ratings AS show_ratings
-ON shows.id = show_ratings.show_id
-GROUP BY shows.title
-ORDER BY rating DESC;
