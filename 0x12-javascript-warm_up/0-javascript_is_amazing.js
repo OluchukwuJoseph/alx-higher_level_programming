0f425e625c71cf91eebf2446d36c57edfa4c6295
Joseph <devjoseph016@gmail.com>
2024-04-25 15:21:33 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-/*
- * This script prints a string (myVar)
- */
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const arrayOfStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < arrayOfStrings.length; i++) {
-  console.log(arrayOfStrings[i]);
-}
