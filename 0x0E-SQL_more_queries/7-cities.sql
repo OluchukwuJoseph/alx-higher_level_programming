50b1bf78202647f26b7def3710c83fa8cf95c5ce
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-05-23 04:06:07 +0100

    Resolved issue in Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,5 +9,5 @@ IF NOT EXISTS cities (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES state.id
+	FOREIGN KEY(state_id) REFERENCES states(id)
 );
