5faaed611ee4f49b1b08b35dabfe6ea847194b2a
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-06-24 12:30:32 +0100

    Added files for Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+    This script takes in an argument and displays all values in the `states`
+    table where `name` matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    # Retrieve Command line Arguments
+    argument = sys.argv[4].split(';')
+    # Establish database connection
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    # Get and Execute query
+    cursor = db.cursor()
+    query = """
+        SELECT *
+        FROM states
+        WHERE name LIKE BINARY '{}'
+        ORDER BY id ASC
+    """.format('%' + argument[0].strip("\'\"") + '%')
+
+    cursor.execute(query)
+
+    # Get and print results
+    rows = cursor.fetchall()
+    if (rows):
+        for row in rows:
+            print(row)
+
+    # Close Connection
+    cursor.close()
+    db.close()
