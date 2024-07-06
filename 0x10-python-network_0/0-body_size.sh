0e747dfe087098cdfc827faccd6c4f69ac0f61f1
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-06 17:46:56 +0100

    Resolved issue in Task 0, 1, 2 and 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
+# This script that takes in a URL, sends a request to that URL and displays the size of the body of the response
 curl -s -o /dev/null -w "%{size_download}\n" "$1"
