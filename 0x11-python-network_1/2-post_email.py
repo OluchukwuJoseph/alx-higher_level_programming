ff1ad8722f870561245f55449ec1ec55ffdbe4c5
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-10 18:34:09 +0100

    Resolved issue in Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,5 +15,4 @@ if __name__ == '__main__':
 
     req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
-        body = response.read()
-        print(body)
+        print(response.read().decode('utf-8'))
